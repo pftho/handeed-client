21d1aceb9156855470549bc184caeed5005374bc
Pauline Thomas <pauline.farina90@gmail.com>
2022-09-08 19:40:56 +0200

    add signup form input, label, state, handle function

--- src/pages/Auth/LoginPage.js
+++ src/pages/Auth/LoginPage.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { useState } from 'react';
 
 function LoginPage() {
-    return <div>LoginPage</div>;
+    //States
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    //handlers
+    const handleChange = (e) => {
+        console.log(e.target.value);
+    };
+
+    return (
+        <div>
+            <h1>Log in</h1>
+            <label for="email">Email</label>
+            <input name="email" type="email" required onChange={handleChange} />
+            <label for="password">Password</label>
+            <input
+                name="password"
+                required
+                type="password"
+                onChange={handleChange}
+            />
+        </div>
+    );
 }
 
 export default LoginPage;
