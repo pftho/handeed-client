ba00cac37eabc3017d2b32f7de7292b501b070ce
Pauline Thomas <pauline.farina90@gmail.com>
2022-09-08 19:07:58 +0200

    Navbar component update using AuthContex

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
+import { AuthContext } from '../context/auth.context';
 
 function Navbar() {
+    const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+
     return (
         <nav>
-            <Link to="/auth/signup">
-                <button>Sign Up</button>
-            </Link>
-            <Link to="/auth/login">
-                <button>Login</button>
-            </Link>
+            {isLoggedIn && (
+                <>
+                    <Link to="/">
+                        <button>Our Ads</button>
+                    </Link>
+                    <div>
+                        <p>credit</p>
+                    </div>
+                    <Link to="/">
+                        <button>Messages</button>
+                    </Link>
+                    <div class="dropdown">
+                        <button class="dropbtn">
+                            {user.name}
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="dropdown-content">
+                            <Link to="#">Profile</Link>
+                            <Link to="#">My ads</Link>
+                            <Link to="#">Log out</Link>
+                        </div>
+                    </div>
+                </>
+            )}
+            {!isLoggedIn && (
+                <>
+                    <Link to="/auth/signup">
+                        <button>Sign Up</button>
+                    </Link>
+                    <Link to="/auth/login">
+                        <button>Login</button>
+                    </Link>
+                </>
+            )}
         </nav>
     );
 }
