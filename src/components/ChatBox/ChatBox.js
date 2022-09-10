9968af91747220f18f3976ab73816eb061169eab
Pauline Thomas <pauline.farina90@gmail.com>
2022-09-10 21:18:19 +0200

    basic idea for chat

---
+++ src/components/ChatBox/ChatBox.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function ChatBox() {
+const []
+
+    return (
+        <div>
+            <div>
+                <h2></h2>
+            </div>
+            <div>
+                <div></div>
+
+                <form>
+                    <input type="text" name="message" />
+                    <button type="submit">Send</button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default ChatBox;
---
+++ src/pages/Message/MessagePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChatBox from '../../components/ChatBox/ChatBox';
+
+function MessagePage() {
+    return (
+        <div>
+            <h1> Messages</h1>
+            <ChatBox />
+        </div>
+    );
+}
+
+export default MessagePage;
